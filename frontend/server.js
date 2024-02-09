812e60e2e164487808854bc0bf12433696cd144e
Ahmed Alam <ahmedalam@Ahmeds-MacBook-Pro.fritz.box>
2024-02-09 17:34:48 +0100

    Fix frontend

--- frontend/server.js
+++ frontend/server.js
@@ -1,14 +1,9 @@
-config = {
-    backend: 'http://127.0.0.1:8000/items',
-    authToken: "test"
-}
-
 const updateData = (imageLink, callback) => {
     // Create a new XMLHttpRequest object
     const xhr = new XMLHttpRequest();
 
     // Open a connection to the server
-    xhr.open('POST', config.backend, true);
+    xhr.open('POST', 'https://classificationofnumberbackend.onrender.com/', true);
 
     // Set the request headers
     xhr.setRequestHeader('Content-Type', 'application/json');
