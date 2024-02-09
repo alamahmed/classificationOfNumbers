9edb6f3144a8fec66741c9bcd93f45f0fe699399
Ahmed Alam <ahmedalam@Ahmeds-MacBook-Pro.fritz.box>
2024-02-09 17:27:21 +0100

    Add requirements

--- frontend/server.js
+++ frontend/server.js
@@ -1,9 +1,14 @@
+config = {
+    backend: 'http://127.0.0.1:8000/items',
+    authToken: "test"
+}
+
 const updateData = (imageLink, callback) => {
     // Create a new XMLHttpRequest object
     const xhr = new XMLHttpRequest();
 
     // Open a connection to the server
-    xhr.open('POST', 'http://127.0.0.1:8000/items', true);
+    xhr.open('POST', config.backend, true);
 
     // Set the request headers
     xhr.setRequestHeader('Content-Type', 'application/json');
